0c2887406b3482bbfaa3589d55b4d4dfd8d2980e
Johnson-f <johnsonnifemi52@gmail.com>
2025-06-14 16:55:06 +0100

    Exclude server folder from Git tracking

--- server/03_INDEXES/02_option.sql
+++
@@ -1,6 +0,0 @@
--- Index for performance
-CREATE INDEX IF NOT EXISTS idx_option_table_is_win ON option_table (is_win);
-
-CREATE INDEX IF NOT EXISTS idx_option_table_net_profit ON option_table (net_profit_dollars);
-
-CREATE INDEX IF NOT EXISTS idx_option_table_holding_period ON option_table (holding_period);
